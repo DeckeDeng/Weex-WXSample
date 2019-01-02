18204148d89345342d625c07737d59505d50d065
三露 <xiaojia.dxj@alibaba-inc.com>
2019-01-02 17:04:41 +0800

    完善imageAdapter

--- app/src/main/java/auction/taobao/com/weexdemoproject/MyApplication.java
+++ app/src/main/java/auction/taobao/com/weexdemoproject/MyApplication.java
@@ -6,6 +6,15 @@ import com.taobao.weex.InitConfig;
 import com.taobao.weex.WXSDKEngine;
 
 
+/**
+ * 注意要在Manifest中设置android:name=".WXApplication"
+ * 要实现ImageAdapter 否则图片不能下载
+ * gradle 中一定要添加一些依赖，否则初始化会失败。
+ * compile 'com.android.support:recyclerview-v7:23.1.1'
+ * compile 'com.android.support:support-v4:23.1.1'
+ * compile 'com.android.support:appcompat-v7:23.1.1'
+ * compile 'com.alibaba:fastjson:1.1.45'
+ */
 public class MyApplication extends Application {
 
     @Override
